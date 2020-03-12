561d46cb9e99d96067981f4ce686d1f5cf11c42f
Hamicch <47416177+Hamicch@users.noreply.github.com>
2020-03-12 19:17:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,9 @@
         },
         methods: {
               submit () {
-                      this.displayFullname = 'Full Name: ' + this.fullName
-                      this.displayEmail ='Email Address: ' + this.email
-                      this.displayPhone ='Phone Number: '  + this.phone
+                      this.displayFullname = `Full Name: ${this.fullName}`
+                      this.displayEmail =`Email Address: ${this.email}`
+                      this.displayPhone =`Phone Number: ${this.phone}`
                       this.fullName = null
                       this.email = null
                       this.phone = null
